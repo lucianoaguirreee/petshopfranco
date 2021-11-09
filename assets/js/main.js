0cfbe01bfc76f8792099033a678ffffc099d6ff3
lucianhoff <lucianoaguirre007@gmail.com>
2021-11-09 18:02:48 -0300

    se modifico las cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,11 +11,11 @@ fetch(endPoint)
     dataTipos.forEach((elem) => {
       if (elem.tipo == tipo) {
         cardElement.innerHTML += `<div class="col-4 p-2">
-          <div class="card w-100 p-6 d-flex align-items-center card-border border-2">
-              <img class="lazyloaded"
+          <div class="card w-100 p-6 d-flex align-items-center justify-content-evenly flex-column card-border border-2 card-size">
+              <img class="lazyloaded img-producto"
                   src="${elem.imagen}"
-                  alt="Gaucho Goods Tribal Leather Dog Collar">
-              <div>
+                  alt="${elem.nombre}">
+              <div class="d-flex flex-column justify-content-evenly">
                   <h5 class="card-title fw-bold text-center pt-3 nombre">${elem.nombre}</h5>
                   <p class="card-text text-center precio fw-bold">$ 5000</p>
                   <div class="text-center pb-3">
@@ -23,7 +23,9 @@ fetch(endPoint)
                       <input id="cantidad-${elem._id}" class="text-center" type="number" name="cantidad-id"
                           min="1" max="${elem.stock}" step="1" value="1">
                   </div>
-                  <a id="${elem._id}" class="boton-comprar btn btn-primary bg-custom mb-3">Agregar al carrito</a>
+                  <div class="d-flex align-items-center justify-content-center">
+                    <a id="${elem._id}" class="boton-comprar btn btn-primary bg-custom mb-3">Agregar al carrito</a>
+                  </div>
               </div>
           </div>
       </div>`;
